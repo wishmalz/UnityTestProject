4495700990bf9cfa880cb23486c966309dabfa69
wishexxt <valeriy.bakalenko@gmail.com>
2015-09-24 17:15:06 +0300

    change scenes

---
+++ Assets/Scripts/LoadNewSceneOnClick.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadNewSceneOnClick : MonoBehaviour {
+
+    public void LoadScene(int scene)
+    {
+        Application.LoadLevel(scene);
+    }
+}
