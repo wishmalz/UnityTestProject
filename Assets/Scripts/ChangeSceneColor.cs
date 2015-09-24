5e397486296d91fae68064dcc321d4772905b770
wishexxt <valeriy.bakalenko@gmail.com>
2015-09-25 02:13:11 +0300

    change color added

---
+++ Assets/Scripts/ChangeSceneColor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSceneColor : MonoBehaviour
+{
+    public Color color1 = Color.red;
+    public Color color2 = Color.blue;
+    public float duration = 3.0F;
+
+    Camera camera;
+
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+        camera.clearFlags = CameraClearFlags.SolidColor;
+    }
+
+    public void ChangeColor()
+    {
+        float t = Mathf.PingPong(Time.time, duration) / duration;
+        camera.backgroundColor = Color.Lerp(color1, color2, t);
+    }
+}
