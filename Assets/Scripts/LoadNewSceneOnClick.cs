a44798b7347dda0318bf204c096e3308376209b2
wishexxt <valeriy.bakalenko@gmail.com>
2015-10-01 15:36:37 +0300

    btn click sound fix

--- Assets/Scripts/LoadNewSceneOnClick.cs
+++ Assets/Scripts/LoadNewSceneOnClick.cs
@@ -1,10 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LoadNewSceneOnClick : MonoBehaviour {
+public class LoadNewSceneOnClick : MonoBehaviour
+{
+
+    private AudioSource audioSource;
+    private AudioClip clip;
 
     public void LoadScene(int scene)
     {
+        StartCoroutine(playSoundThenLoad(scene));
+    }
+
+    IEnumerator playSoundThenLoad(int scene)
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(audioSource.clip);
+        yield return new WaitForSeconds(audioSource.clip.length - (float)1.65);  // 1.65 length of silence in sound clip
+
         Application.LoadLevel(scene);
     }
 }
