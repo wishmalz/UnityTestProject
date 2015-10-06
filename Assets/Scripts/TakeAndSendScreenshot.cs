e124b0ecd42aa441143ae929797a2682e00b8413
wishexxt <valeriy.bakalenko@gmail.com>
2015-10-07 00:56:35 +0300

    texture loader

--- Assets/Scripts/TakeAndSendScreenshot.cs
+++ Assets/Scripts/TakeAndSendScreenshot.cs
@@ -3,8 +3,6 @@ using System.Collections;
 using System.IO;
 using System;
 using System.Text;
-using Soomla.Profile;
-using Soomla;
 using UnityEngine.UI;
 
 public class TakeAndSendScreenshot : MonoBehaviour {
@@ -65,8 +63,8 @@ public class TakeAndSendScreenshot : MonoBehaviour {
 
     private Texture2D ChangeScreenshot(Texture2D screenshot)
     {
-        Texture2D imgToAppend = Resources.Load("Images/umbrella1") as Texture2D;
-        Texture2D textToAppend = Resources.Load("Images/example_font_0") as Texture2D;
+        Texture2D imgToAppend = TextureLoader.MakeTexture("Images/umbrella1");//Resources.Load("Images/umbrella1") as Texture2D;
+        Texture2D textToAppend = TextureLoader.MakeTexture("Images/example_font_0"); //Resources.Load("Images/example_font_0") as Texture2D;
         Texture2D changedImg = null;
         int newImgHeight = 0;
         int newImgWidth = 0;
@@ -74,7 +72,7 @@ public class TakeAndSendScreenshot : MonoBehaviour {
         newImgHeight = screenshot.height + imgToAppend.height;
         newImgWidth = screenshot.width;
 
-        changedImg = new Texture2D(newImgWidth, newImgHeight);
+        changedImg = TextureLoader.MakeTexture(newImgWidth, newImgHeight);//new Texture2D(newImgWidth, newImgHeight);
         changedImg.SetPixels(0, changedImg.height - screenshot.height, screenshot.width, screenshot.height, screenshot.GetPixels());    // append sceenshot to new img
         changedImg.SetPixels(0, 0, imgToAppend.width, imgToAppend.height, imgToAppend.GetPixels());     // append custom img to new img
         changedImg.SetPixels(imgToAppend.width, 0, 14, 31, textToAppend.GetPixels(107, textToAppend.height-31, 14, 31));  // add a
@@ -82,41 +80,5 @@ public class TakeAndSendScreenshot : MonoBehaviour {
         changedImg.SetPixels(imgToAppend.width + 14 * 2 - 3, 0, 14, 31, textToAppend.GetPixels(30, textToAppend.height - 31, 14, 31));   // add 1
 
         return changedImg;
-    }
-
-    private IEnumerator ShareScreenshot(byte[] screenshot)
-    {
-        isProcessing = true;
-
-        byte[] dataToSave = new byte [screenshot.Length];
-        Array.Copy(screenshot, dataToSave, screenshot.Length);
-
-        string destination = Path.Combine(Application.persistentDataPath, "Screenshot.png");
-        FileStream fs = new FileStream(destination, FileMode.OpenOrCreate);
-        BinaryWriter ww = new BinaryWriter(fs);
-        ww.Write(screenshot);
-        ww.Close();
-        fs.Close();
-        Debug.Log("WRITE IMAGE TO DISK");
-        GameObject.Find("pathText").GetComponent<Text>().text = "File is on disk";
-        AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
-            AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
-            intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string> ("ACTION_SEND"));
-            AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
-            AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("parse", "file://" + destination);
-            intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_STREAM"), uriObject);
-            intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), shareText + gameLink);
-            intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_SUBJECT"), subject);
-            intentObject.Call<AndroidJavaObject>("setType", "image/png");
-            AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-            AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
-
-            currentActivity.Call("startActivity", intentObject);
-        Debug.Log("activity started");
-        GameObject.Find("pathText").GetComponent<Text>().text = "Activity";
-
-        isProcessing = false;
-
-        yield return new WaitForSeconds(1);
     }
 }
---
+++ Assets/Scripts/TextureLoader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class TextureLoader{
+
+    public static Texture2D MakeTexture(string texturePath)
+    {
+        Texture2D texture = Resources.Load(texturePath) as Texture2D;
+
+        return texture;
+    }
+
+    public static Texture2D MakeTexture(int newImgWidth, int newImgHeight)
+    {
+        Texture2D texture = new Texture2D(newImgWidth, newImgWidth);
+
+        return texture;
+    }
+
+}
